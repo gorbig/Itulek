2ed96a99cbe8ddcfa261aa29af8cd682f655d49b
reiss <ilyas.abdrakhmanov07@gmail.com>
2025-10-08 16:16:19 +0400

    Create project

---
+++ src/main/java/org/example/itulek/ItulekApplication.java
@@ -0,0 +1,13 @@
+package org.example.itulek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItulekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItulekApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/itulek/ItulekApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.itulek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItulekApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
